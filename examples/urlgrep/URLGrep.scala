5c353ebe57654fd035dd54be9e0bf59f9ddd4e81
Przemek Pokrywka <przemyslaw.pokrywka@hellosoda.com>
2021-01-04 19:47:49 +0000

    URLGrep example

---
+++ examples/urlgrep/URLGrep.scala
@@ -0,0 +1,15 @@
+// 2> /dev/null; source $(curl -sL https://git.io/boot-tsk | sh); run; exit
+
+object URLGrep {
+
+  def main(args: Array[String]) = {
+    val urls = for {
+      line  <- io.Source.stdin.getLines()
+      token <- line.split("\\s+")
+      clean <- token.split("[\"']")
+      if util.Try(new java.net.URL(clean)).isSuccess
+    } yield clean
+    urls.toSet.toSeq.sorted.foreach(println)
+  }
+
+}
