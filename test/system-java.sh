638df9383d0cbb0ac590e757cdd27e9bfd466341
Przemek Pokrywka <przemyslaw.pokrywka@hellosoda.com>
2021-07-12 20:33:21 +0100

    addressed some other shellcheck issues

--- test/system-java.sh
+++ test/system-java.sh
@@ -8,14 +8,18 @@ testUsingSystemJavaShouldNotDownloadJVM() {
 
   (# Using a sub-shell to keep these variables scoped
     export JAVA_HOME="$javaHome"
+    # shellcheck disable=SC2030
     export PATH="$javaHome/bin:$PATH"
     export HOME="$homeDir"
     export COURSIER_JVM_CACHE="$coursierJvms"
+    # shellcheck disable=SC2164 # wd is known to exist
     cd "$wd"
     bash -c "${wd}/Easy.scala" > "${standard_output_file}" 2> "${standard_error_file}"
 
     assertStandardOutputEquals "ok"
+    # shellcheck disable=SC2012
     assertEquals "Should have installed coursier without jvm" "1" "$(ls -1 "$HOME"/.tsk/cs-*-jvmless | wc -l)"
+    # shellcheck disable=SC2012
     assertEquals "Should not have download any jvm" "0" "$(ls -1 "$COURSIER_JVM_CACHE"/ | wc -l)"
   )
 
